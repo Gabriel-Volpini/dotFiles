69c0a8e2241288690f3c52f6346be5bd2f5c8ab5
Gabriel Volpini <gabrielvolpini@192.168.0.6>
2025-04-11 16:54:26 -0300

    Feat, try catck error temp

--- widgets/Temperature.jsx
+++ widgets/Temperature.jsx
@@ -5,99 +5,116 @@ export const command = `curl -s 'wttr.in/Belo+Horizonte?format=%t,%C'`;
 export const refreshFrequency = 10 * 60 * 1000; // 10 minutos
 
 export const render = ({ output }) => {
-    const [temp, rawCondition] = output.trim().split(",");
-    const condition = rawCondition.toLowerCase();
+    try{
+        const [temp, rawCondition] = output.trim().split(",");
+        const condition = rawCondition.toLowerCase();
 
-    let icon;
-    if (condition.includes("sun")) {
-        icon = WeatherIcons.sun;
-    } else if (condition.includes("cloud")) {
-        icon = WeatherIcons.cloud;
-    } else if (condition.includes("rain")) {
-        icon = WeatherIcons.rain;
-    } else if (condition.includes("storm")) {
-        icon = WeatherIcons.storm;
-    } else {
-        icon = WeatherIcons.default;
-    }
+        let icon;
+        if (condition.includes("sun")) {
+            icon = WeatherIcons.sun;
+        } else if (condition.includes("cloud")) {
+            icon = WeatherIcons.cloud;
+        } else if (condition.includes("rain")) {
+            icon = WeatherIcons.rain;
+        } else if (condition.includes("storm")) {
+            icon = WeatherIcons.storm;
+        } else {
+            icon = WeatherIcons.default;
+        }
 
-    return (
-        <div className={centralizedContainer}>
-            <div className={container}>
-                <div className={box}>
-                    <div className={dataWrapper}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 512 512" >
-                            {icon}
-                        </svg>
-                        <span className={tempStyle}>{temp}</span>
+        return (
+            <div className={centralizedContainer}>
+                <div className={container}>
+                    <div className={box}>
+                        <div className={dataWrapper}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 512 512" >
+                                {icon}
+                            </svg>
+                            <span className={tempStyle}>{temp}</span>
+                        </div>
+                        <span className={rawStyle}>{rawCondition}</span>
                     </div>
-                    <span className={rawStyle}>{rawCondition}</span>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } catch(msg){
+        console.log({msg, output})
+
+        return (
+            <div className={centralizedContainer}>
+                <div className={container}>
+                    <div className={box}>
+                        <span className={rawStyle}>DEU RUIM</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 const centralizedContainer = css`
-    box-sizing: content-box;
-    @font-face {
-        font-family: "Antikor";
-        src: url("Antikor.ttf");
-    }
-    font-family: "Antikor";
+box-sizing: content-box;
+@font-face {
+font-family: "Antikor";
+src: url("Antikor.ttf");
+}
+font-family: "Antikor";
 
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+position: fixed;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
 
-    width: 60%;
-    height: 80%;
+width: 60%;
+height: 80%;
 `
 
 const container = css`
-    width:100%;
-    height:100%;
-    position:relative;
+width:100%;
+height:100%;
+position:relative;
 `
 const box = css`
-    background: #181825;
-    //opacity:0.8;
+background: #181825;
+max-width: 20%;
+min-width: 20%;
+overflow:hidden;
+//opacity:0.8;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap:10px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap:10px;
 
-    position:absolute;
-    top:0;
-    left: 138px;
+position:absolute;
+top:0;
+left: 138px;
 
-    height:80px;
-    padding:18px;
-    overflow:hidden;
+height:80px;
+padding:18px;
+overflow:hidden;
 
-    border-radius: 20px;
-    border: 5px solid #b4befe;
+border-radius: 20px;
+border: 5px solid #b4befe;
 `
 
 const tempStyle = css`
-    font-size: 38px;
-    color: #a6e3a1;
-    padding-top: 10px
+font-size: 38px;
+color: #a6e3a1;
+padding-top: 10px
 `
 
 const dataWrapper = css`
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    flex-direction: row;
-    gap:20px;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction: row;
+gap:20px;
 `
 const rawStyle = css`
-    font-size: 18px;
-    color:#f5e0dc;
+font-size: 18px;
+color:#f5e0dc;
 `
 
 const WeatherIcons = {
