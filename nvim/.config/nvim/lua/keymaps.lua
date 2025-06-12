7bfb9374c949b6b6c201a42064358537f30903ef
Gabriel Volpini <gabrielvolpini@192.168.0.21>
2025-06-11 23:05:51 -0300

    Feat: add split keymap vim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -27,9 +27,9 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 -- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
@@ -56,5 +56,11 @@ vim.api.nvim_set_keymap("n", "<A-j>", "<C-w>j", { noremap = true })
 vim.api.nvim_set_keymap("n", "<A-k>", "<C-w>k", { noremap = true })
 vim.api.nvim_set_keymap("n", "<A-l>", "<C-w>l", { noremap = true })
 
---Buffer
+--Alternar entre buffers
 vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>b#<CR>", { noremap = true })
+
+--usar jq para formatar arquivo json
+vim.keymap.set("n", "gj", "<cmd>%!jq '.'<cr>", { noremap = true, silent = true })
+
+--lsp error hover
+vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { silent = true, desc = "Mostrar erro no cursor" })
