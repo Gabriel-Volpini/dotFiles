4089a7798c8b2dea1f9adb4601b10da5453c7de6
Gabriel Volpini <gabriel.volpini@ideliverthat.com>
2025-11-08 01:19:12 -0300

    feat

--- nvim/.config/nvim/lua/plugins/custom/init.lua
+++ nvim/.config/nvim/lua/plugins/custom/init.lua
@@ -1,27 +1,30 @@
 local M = {}
 
 function M.setup()
-  require("plugins.custom.custom.project").setup({
+  ------------------------------------------------------------------
+  local projects = require("plugins.custom.custom.project")
+  ------------------------------------------------------------------
+  projects.setup({
     projects = {
       { name = "DT - Backend - Deliveries Api", dir = "~/Documents/deliverThat/Delivery-API/apps/deliveries-api/" },
       { name = "DT - Backend - Hermes", dir = "~/Documents/deliverThat/Delivery-API/apps/hermes/" },
       { name = "DT - Backend - Root", dir = "~/Documents/deliverThat/Delivery-API/" },
       { name = "DT - Frontend - Mobile App", dir = "~/Documents/deliverThat/DeliverThat-rn/" },
       { name = "Memora", dir = "~/Documents/private_repos/LifeApp/mobile/" },
-      { name = "Settings", dir = "~/Documents/dotFiles/nvim/.config/nvim/" },
-      { name = "Project sessions", dir = "/Users/gabrielvolpini/.local/share/nvim/project_sessions" },
+      { name = "Settings", dir = "~/Documents/dotFiles/nvim/.config/nvim/lua/" },
+      { name = "Project sessions", dir = "~/.local/share/nvim/project_sessions" },
       { name = "Opencode agents", dir = "~/.config/opencode/agent/" },
     },
   })
 
+  ------------------------------------------------------------------
   local helpers = require("plugins.custom.modules.Helpers")
+  ------------------------------------------------------------------
   vim.keymap.set("n", "<leader>r", helpers.run_and_show, { desc = "Run Lua file and display return value" })
 end
 
 return {
   name = "custom",
   dir = vim.fn.stdpath("config") .. "/lua/plugins/custom/",
-  config = function()
-    M.setup()
-  end,
+  config = M.setup(),
 }
